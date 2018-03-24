d4513fb2ca68f65c2a86ebcbd4de0ef1734071a9
stevieb48 <seb48@students.uwf.edu>
2018-03-24 16:27:25 -0500

    Add project files.

---
+++ DBComm/DBComm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+
+/*
+ * This class creates a database connection to an Access database and has various methods 
+ * to retrieve information or manipulate the database.
+ *
+ * author: Group 7 (Stephen Bailey, Omar Garcia, Craig Wyse, Matthew Harris)
+ * course: SEII
+ * assignment: InRealLife (Group Project Spring 2018)
+ * date: 03/20/2018
+ * file name: DBComm.cs
+ * version: 1.0
+ */
+namespace InRealLife_2
+{
+    public class DBComm
+    {
+        // connection string
+        OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\ScenarioData.accdb");
+
+        //
+        public String displayScenarioTitle(String scTitle)
+        {
+            DataSet ds = new DataSet();
+            string query = "SELECT ScenarioName FROM Scenario Where ScenarioName = " + scTitle;
+            using (conn)
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+            {
+                adapter.Fill(ds);
+                return ds.ToString();
+            }
+        }
+
+        //
+        public String displayScenarioDescription(String scTitle)
+        {
+            DataSet ds = new DataSet();
+            String query = "SELECT ScenarioDesc FROM Scenario Where ScenarioName = " + scTitle;
+            using (conn)
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+            {
+                adapter.Fill(ds);
+                return ds.ToString();
+            }
+
+        }
+
+        //
+        public DataTable displayAllScenariosByTitle()
+        {
+            DataTable dt = new DataTable();
+            String query = "SELECT ScenarioName FROM Scenario";
+            using (conn)
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+            {
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        /*
+         * 
+         * SELECT Scenario.ScenarioName, Stage.StageDescription, Answer.AnswerDescription, Answer.nextStageID
+         * FROM (Scenario INNER JOIN Stage ON Scenario.[ScenarioID] = Stage.[ScenarioID]) INNER JOIN Answer ON Stage.[StageID] = Answer.[StageID]
+         * 
+         * 
+         */
+        public DataTable displayRunningScenario()
+        {
+            DataTable dt = new DataTable();
+            String query = "SELECT ScenarioName, StageDescription, AnswerDescription, nextStageID * FROM(Scenario INNER JOIN Stage ON Scenario.[ScenarioID] = Stage.[ScenarioID]) INNER JOIN Answer ON Stage.[StageID] = Answer.[StageID]";
+            using (conn)
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+            {
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        // method to get scenarios
+        public int DeleteSelectedScenario(int lstScenariosSelectedIndex)
+        {
+            // create return variable for number of rows set to zero
+            int scenarioRowsDeleted = 0;
+
+            String query = "DELETE * FROM Scenario WHERE ScenarioID = " + lstScenariosSelectedIndex;
+            using (conn)
+            using (OleDbCommand Cmd = new OleDbCommand(query, conn))
+            {
+                scenarioRowsDeleted = Cmd.ExecuteNonQuery();
+            }
+
+            // return number of rows deleted
+            return scenarioRowsDeleted;
+        }
+    }
+}
---
+++ InRealLife_2/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * This GUI is the main menu for the scenario builder which allows the user to create a scenario form, edit a scenario form, 
+ * delete an entire scenario, or preview a scenario.
+ *
+ * author: Group 7 (Stephen Bailey, Omar Garcia, Craig Wyse, Matthew Harris)
+ * course: SEII
+ * assignment: InRealLife (Group Project Spring 2018)
+ * date: 03/20/2018
+ * file name: MainWindow.xaml.cs
+ * version: 1.0
+ */
+namespace InRealLife_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBComm newDBComm = new DBComm();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeForm();
+        }
+
+        // initialize the form
+        private void InitializeForm()
+        {
+            // reset list box
+            lstvwScenarios.Items.Clear();
+
+            // enable create button
+            btnCreateScenario.IsEnabled = true;
+                
+            //
+            DataTable returnedScenarioTable = newDBComm.displayAllScenariosByTitle();
+
+            // if data table has rows
+            if (returnedScenarioTable.Rows.Count > 0)
+            {
+                // add data to listbox
+                AddDataToListBox(returnedScenarioTable);
+            }
+            else
+            {
+                // else scenario list is empty
+                ScenarioListIsEmpty();
+            }
+        }
+
+        // exit builder button click event UNDER DEVELOPMENT
+        private void BtnExitBuilder_Click(object sender, RoutedEventArgs e)
+        {
+            // hide current form
+            // this.Hide();
+        }
+
+        // create scenario button click event
+        private void BtnCreateScenario_Click(object sender, RoutedEventArgs e)
+        {
+            // hide current form
+            // this.Hide();
+
+            // load form to edit scenario
+            // .show();
+
+        }
+
+        // delete scenario button click event
+        private void BtnDeleteScenario_Click(object sender, RoutedEventArgs e)
+        {
+            //
+            int scenarioRowsDeleted = 0;
+
+            // run query to delete selected scenario from DB
+            scenarioRowsDeleted = newDBComm.DeleteSelectedScenario(lstvwScenarios.SelectedIndex);
+
+            // if scenario was deleted
+            if (scenarioRowsDeleted > 0)
+            {
+                // Show user which scenario was deleted
+                MessageBox.Show("The scenario called " + lstvwScenarios.SelectedValue + " was deleted");
+
+                // reset form
+                InitializeForm();
+            }
+            else
+            {
+                // Show user error while scenario was deleted
+                MessageBox.Show("Error deleting scenario");
+
+                // reset form
+                InitializeForm();
+            }
+        }
+
+        // preview scenario button click event
+        private void BtnPreviewScenario_Click(object sender, RoutedEventArgs e)
+        {
+            // hide current form
+            // this.Hide();
+
+            // load form to preview scenario
+
+        }
+
+        // method for form behaviors if list is empty
+        private void ScenarioListIsEmpty()
+        {
+            btnEditScenario.IsEnabled = false;
+            btnDeleteScenario.IsEnabled = false;
+            btnPreviewScenario.IsEnabled = false;
+        }
+
+        // method for form behaviors if list has data
+        private void ScenarioListHasValues()
+        {
+            btnEditScenario.IsEnabled = true;
+            btnDeleteScenario.IsEnabled = true;
+            btnPreviewScenario.IsEnabled = true;
+        }
+
+        // method to add scenario data to scenario list box
+        private void AddDataToListBox(DataTable returnedScenarioTable)
+        {
+            //
+            
+
+            // loop to put scenario names from data table into scenario listbox items
+            for (int i = 0; i < returnedScenarioTable.Rows.Count; i++)
+            {
+                //listView1.Items.Add(dt);
+                lstvwScenarios.Items.Add(returnedScenarioTable.Rows[i][0].ToString());
+            }
+        }
+
+        // has scenarios listbox selection changed
+        private void LstvwScenarios_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // enable buttons
+            ScenarioListHasValues();
+
+        }
+    }
+}
